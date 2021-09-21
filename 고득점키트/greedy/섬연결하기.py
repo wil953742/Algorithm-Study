d533eb3a261f1106ca02b0761c42101f2e0561ba
unknown <wil953742@gmail.com>
2021-09-21 18:45:55 +0900

    kruskal 알고리즘 문제: 섬 연결하기 완료

--- 고득점키트/greedy/섬연결하기.py
+++ 고득점키트/greedy/섬연결하기.py
@@ -1,6 +1,26 @@
 def solution(n, costs):
-    costs.sort(key = lambda x: x[2], reverse=True)
-    return costs
+    
+    answer = 0
+
+    def find(c):
+        if par[c] == c:
+            return c
+        else:
+            return find(par[c])
+
+    def union(a, b):
+        a, b = find(a), find(b)
+        par[a] = b
+
+    par = [i for i in range(n)]
+
+    costs.sort(key = lambda x: x[2])
+    for i in costs:
+        if find(i[0]) != find(i[1]):
+            answer += i[2]
+            union(i[0], i[1])
+        
+    return answer
 
 sol = solution(4, [[0,1,1],[0,2,2],[1,2,5],[1,3,1],[2,3,8]])
 print(sol) #4
