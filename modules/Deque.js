3fde679f75793df791e5448a7e97b55f9aecc5d4
J045_김웅일 <66904178+wil953742@users.noreply.github.com>
2021-12-11 00:31:00 +0900

    DLL을 사용한 Deque 자료구조 구현

---
+++ modules/Deque.js
@@ -0,0 +1,80 @@
+class Node {
+  constructor(value) {
+    this.next = null;
+    this.prev = null;
+    this.value = value;
+  }
+}
+
+class Deque {
+  length;
+  #head;
+  #tail;
+  constructor(list = []) {
+    this.length = list.length;
+    this.#initDeque(list);
+  }
+
+  #initDeque(list) {
+    if (!list) {
+      this.#head = null;
+      this.#tail = null;
+    } else {
+      list.forEach((value) => {
+        this.push(value);
+      });
+    }
+  }
+
+  push(value) {
+    const node = new Node(value);
+    if (!this.length) this.#head = node;
+    else {
+      node.prev = this.#tail;
+      this.#tail.next = node;
+    }
+    this.#tail = node;
+    this.length++;
+  }
+
+  pushleft(value) {
+    const node = new Node(value);
+    if (!this.length) this.#tail = node;
+    else {
+      node.next = this.#head;
+      this.#head.prev = node;
+    }
+    this.#head = node;
+    this.length++;
+  }
+
+  pop() {
+    if (!this.length) return null;
+    const value = this.#tail.value;
+    this.length--;
+    if (!this.length) this.#initDeque([]);
+    else {
+      this.#tail = this.#tail.prev;
+      this.#tail.next = null;
+    }
+    return value;
+  }
+
+  popleft() {
+    if (!this.length) return null;
+    const value = this.#head.value;
+    this.length--;
+    if (!this.length) this.#initDeque([]);
+    else {
+      this.#head = this.#head.next;
+      this.#head.prev = null;
+    }
+    return value;
+  }
+
+  isEmpty() {
+    return this.length === 0;
+  }
+}
+
+module.exports = Deque;
